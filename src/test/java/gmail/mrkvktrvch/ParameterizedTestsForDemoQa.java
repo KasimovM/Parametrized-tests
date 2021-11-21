ab44aeca5516b3b993cbb29f142e7cddda14a977
so6er <mrkvktrvch@gmail.com>
2021-11-21 11:36:26 +0300

    finished my parameterized tests

--- src/test/java/gmail/mrkvktrvch/ParameterizedTestsForDemoQa.java
+++ src/test/java/gmail/mrkvktrvch/ParameterizedTestsForDemoQa.java
@@ -1,11 +1,13 @@
 package gmail.mrkvktrvch;
+
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,8 +16,13 @@ public class ParameterizedTestsForDemoQa {
 
     @DisplayName("Поиск корма различных брэндов")
     @ParameterizedTest(name = "Проверка отображения корма брэнда {0}")
-    @ValueSource(strings = {"Almo Nature", "Applaws"})
+    @ValueSource(strings = {
+            "Almo Nature",
+            "Applaws"
+    })
+
     void searchFeedFromDifferentBrandsTest(String brandName) {
+
         open("https://www.petshop.ru/");
         $(".MuiInputBase-input").setValue(brandName).pressEnter();
         $("#catalogue-wrapper").shouldHave(text(brandName));
@@ -26,12 +33,12 @@ public class ParameterizedTestsForDemoQa {
     @ParameterizedTest(name = "Внесение информации о сотруднике {1} {0}")
     @CsvSource(value = {
             "Mark | Kasimov | mrkvktrvch@gmail.com | 28 | 100500 | IT",
-            "Ivan | Petrov | yasobaka@mail.com | 35 |50000| Security"
-    },
+            "Ivan | Petrov | yasobaka@mail.com | 35 |50000| Security"},
             delimiter = '|')
 
     void addNewEmployeeTest(String inputFirstname, String inputLastName, String inputEmail, String inputAge, String inputSalary,
                             String inputDepartment) {
+
         Selenide.open("https://demoqa.com/webtables");
         $("#addNewRecordButton").click();
         $("#firstName").setValue(inputFirstname);
@@ -48,7 +55,9 @@ public class ParameterizedTestsForDemoQa {
     @EnumSource(UsersList.class)
     @DisplayName("Внесение информации о новом сотруднике в таблицу")
     @ParameterizedTest(name = "Внесение информации о сотруднике {0}")
+
     void addNewEmployeeTestWithEnumSource(UsersList usersList) {
+
         Selenide.open("https://demoqa.com/webtables");
         $("#addNewRecordButton").click();
         $("#firstName").setValue(usersList.getName());
@@ -60,5 +69,31 @@ public class ParameterizedTestsForDemoQa {
         $("#submit").click();
         $(".rt-table").shouldHave(text(usersList.getName()), text(usersList.getLastName()), text(usersList.getEmail()),
                 text(usersList.getAge()), text(usersList.getSalary()), text(usersList.getDepartment()));
+    }
+
+    static Stream<Arguments> addNewEmployeeTestWithMethodSource() {
+        return Stream.of(
+                Arguments.of("Mark", "Kasimov", "mrkvktrvch@gmail.com", "28", "100500", "IT"),
+                Arguments.of("Ivan", "Petrov", "yasobaka@mail.ru", "35", "50000", "Security")
+        );
+    }
+
+    @MethodSource
+    @DisplayName("Внесение информации о новом сотруднике в таблицу")
+    @ParameterizedTest(name = "Внесение информации о сотруднике {1} {0}")
+
+    void addNewEmployeeTestWithMethodSource(String name, String lastname, String email, String age, String salary,
+                                            String department) {
+
+        open("https://demoqa.com/webtables");
+        $("#addNewRecordButton").click();
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(lastname);
+        $("#userEmail").setValue(email);
+        $("#age").setValue(age);
+        $("#salary").setValue(salary);
+        $("#department").setValue(department);
+        $("#submit").click();
+        $(".rt-table").shouldHave(text(name), text(lastname), text(email), text(age), text(salary), text(department));
     }
 }
